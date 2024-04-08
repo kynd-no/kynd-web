188a1c910dc0bcbc3d39ba6cdcd54a10826819f6
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2024-04-08 12:31:37 +0200

    Remove base from url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kynd-no.github.io',
-  base: '/kynd-web',
+  site: 'https://kynd.no',
   integrations: [mdx(), sitemap()],
   vite: {
     envDir: '..',
