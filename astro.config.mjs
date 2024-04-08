e350064aef95ff89e87ddc100dc4e52f2fa93cd9
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2024-04-08 11:12:07 +0200

    Add sass and new css

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,4 +7,18 @@ export default defineConfig({
   site: 'https://kynd-no.github.io',
   base: '/kynd-web',
   integrations: [mdx(), sitemap()],
+  vite: {
+    envDir: '..',
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData(source, fp) {
+            if (fp.endsWith('main.scss')) return source;
+            if (fp.endsWith('variables.scss')) return source;
+            return `@import "src/styles/main.scss"; ${source}`;
+          },
+        },
+      },
+    },
+  },
 });
