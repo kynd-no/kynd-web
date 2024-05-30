3cce2e4cb1ee32514c73ea09246d12e263990dd1
Frederik Gørvell de Lichtenberg <frederikgdl@gmail.com>
2024-05-30 18:12:31 +0200

    Add image component and other fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,7 @@ const blog = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.coerce.date().optional(),
+    pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
--- src/content/employee/axel.md
+++ src/content/employee/axel.md
@@ -1,6 +1,6 @@
 ---
 name: 'Axel'
-image: '/blog-placeholder-3.jpg'
+image: '/axel.jpeg'
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus nec neque ac tristique. Mauris egestas vestibulum orci, nec placerat.
